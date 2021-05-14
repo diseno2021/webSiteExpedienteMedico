8ca27d2f005ff95433610ca4390095657203a83d
armandoherrer107 <armandojoseesperanzaherrera@gmail.com>
2021-05-14 10:30:32 -0600

    primer prueba

---
+++ src/pages/SitioWeb.vue
@@ -0,0 +1,241 @@
+<template>
+  
+    <q-layout view="lHh lpr lFf" class="shadow-2 rounded-borders">
+      <q-header elevated>
+        <q-toolbar class="bg-primary text-white">
+          <q-btn flat round dense icon="menu" class="q-mr-sm" />
+          <q-avatar>
+            <img src="./../assets/doctoresf.png">
+          </q-avatar>
+
+          <q-toolbar-title>VIRTUALDOC</q-toolbar-title>
+        <q-btn flat round dense icon="search" class="q-mr-xs" />
+         
+        </q-toolbar>
+      </q-header>
+
+
+
+      <q-page-container>
+        <q-page class="q-pa-md">
+          <div class="row">
+<div class="col-4 text-center"><a href="">TEMAS DE SALUD</a></div>
+<div class="col-4 text-center">
+<a href="">MEDICINAS Y SUPLEMENTOS</a>
+</div>
+<div class="col-4 text-center"><a href="">PRUEBAS MEDICAS</a></div>
+</div>
+<hr>
+<div class="row">
+<div class="col-12 text-center">
+<q-card class="my-card bg-primary text-white">
+<q-card-section>
+<h3>
+Información sobre COVID-19 Obtenga la información de salud pública
+más reciente de los CDC Entérese sobre lo nuevo en investigación
+de los NIH Encuentre información del HHS sobre el COVID-19 y usted
+</h3>
+</q-card-section>
+</q-card>
+</div>
+</div>
+
+
+<div class="row">
+
+<div class="col-4">
+
+<div class="q-pa-md" style="max-width: 350px">
+<q-list bordered>
+<q-item clickable v-ripple>
+<q-item-section avatar>
+<q-icon color="negative" name="favorite" />
+</q-item-section>
+
+<q-item-section>Temas de salud</q-item-section>
+</q-item>
+<q-item clickable v-ripple>
+<q-item-section avatar>
+<q-icon color="negative" name="local_hospital" />
+</q-item-section>
+
+<q-item-section>Medicinas y suplementos</q-item-section>
+</q-item>
+<q-item clickable v-ripple>
+<q-item-section avatar>
+<q-icon color="negative" name="bloodtype" />
+</q-item-section>
+
+<q-item-section>Pruebas medicas</q-item-section>
+</q-item>
+
+</q-list>
+</div>
+<h4>Siguenos:</h4>
+<div class="row">
+<div class="col-4 text-center"><q-icon color="primary" name="facebook" style="font-size: 2em"/></div>
+<div class="col-4 text-center">
+<q-icon color="negative" name="health_and_safety" style="font-size: 2em"/>
+</div>
+<div class="col-4 text-center"><q-icon color="negative" name="bloodtype" style="font-size: 2em"/></div>
+</div>
+
+</div>
+
+<div class="col-8 q-pa-md justify-center">
+<q-card>
+<q-carousel
+v-model="slide"
+transition-prev="slide-right"
+transition-next="slide-left"
+swipeable
+animated
+control-color="primary"
+navigation
+padding
+arrows
+class="bg-grey-1 shadow-2 rounded-borders"
+>
+<q-carousel-slide :name="1" img-src="../../src/assets/img/doctor-1.jpg" style="height: 280px; max-width: 1100px"/>
+<q-carousel-slide :name="2" img-src="../../src/assets/img/doctor-2.jpg" style="height: 280px; max-width: 1100px"/>
+<q-carousel-slide :name="3" img-src="../../src/assets/img/doctor-3.jpg" style="height: 280px; max-width: 1100px"/>
+</q-carousel>
+</q-card>
+</div>
+
+
+</div>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  
+</template>
+
+
+
+<!-- <template>
+<div>
+<q-toolbar class="bg-negative text-white">
+ <q-btn flat round dense icon="menu" class="q-mr-sm" /> 
+<h1>VIRTUALDOC</h1>
+<q-space />
+<q-btn flat round dense icon="search" class="q-mr-xs" />
+</q-toolbar>
+<q-toolbar inset>
+<q-toolbar-title
+><strong>Informacion de salud para usted</strong></q-toolbar-title
+>
+<q-btn>ACERCA DE SISTEMAS ABC</q-btn>
+<q-btn>Contactenos</q-btn>
+ <q-space/> 
+</q-toolbar>
+</div>
+
+
+
+<div class="row">
+<div class="col-4 text-center"><a href="">Temas de salud</a></div>
+<div class="col-4 text-center">
+<a href="">Medicinas y suplementos</a>
+</div>
+<div class="col-4 text-center"><a href="">Pruebas Medicas</a></div>
+</div>
+<div class="row">
+<div class="col-12 text-center">
+<q-card class="my-card bg-negative text-white">
+<q-card-section>
+<h3>
+Información sobre COVID-19 Obtenga la información de salud pública
+más reciente de los CDC Entérese sobre lo nuevo en investigación
+de los NIH Encuentre información del HHS sobre el COVID-19 y usted
+</h3>
+</q-card-section>
+</q-card>
+</div>
+</div>
+
+
+<div class="row">
+
+<div class="col-4">
+
+<div class="q-pa-md" style="max-width: 350px">
+<q-list bordered>
+<q-item clickable v-ripple>
+<q-item-section avatar>
+<q-icon color="negative" name="favorite" />
+</q-item-section>
+
+<q-item-section>Temas de salud</q-item-section>
+</q-item>
+<q-item clickable v-ripple>
+<q-item-section avatar>
+<q-icon color="negative" name="local_hospital" />
+</q-item-section>
+
+<q-item-section>Medicinas y suplementos</q-item-section>
+</q-item>
+<q-item clickable v-ripple>
+<q-item-section avatar>
+<q-icon color="negative" name="bloodtype" />
+</q-item-section>
+
+<q-item-section>Pruebas medicas</q-item-section>
+</q-item>
+
+</q-list>
+</div>
+<h4>Siguenos:</h4>
+<div class="row">
+<div class="col-4 text-center"><q-icon color="primary" name="facebook" style="font-size: 2em"/></div>
+<div class="col-4 text-center">
+<q-icon color="negative" name="health_and_safety" style="font-size: 2em"/>
+</div>
+<div class="col-4 text-center"><q-icon color="negative" name="bloodtype" style="font-size: 2em"/></div>
+</div>
+
+</div>
+
+<div class="col-8 q-pa-md justify-center">
+<q-card>
+<q-carousel
+v-model="slide"
+transition-prev="slide-right"
+transition-next="slide-left"
+swipeable
+animated
+control-color="primary"
+navigation
+padding
+arrows
+class="bg-grey-1 shadow-2 rounded-borders"
+>
+<q-carousel-slide :name="1" img-src="../../src/assets/img/doctor-1.jpg" style="height: 280px; max-width: 1100px"/>
+<q-carousel-slide :name="2" img-src="../../src/assets/img/doctor-2.jpg" style="height: 280px; max-width: 1100px"/>
+<q-carousel-slide :name="3" img-src="../../src/assets/img/doctor-3.jpg" style="height: 280px; max-width: 1100px"/>
+</q-carousel>
+</q-card>
+</div>
+
+
+</div>
+
+
+
+
+
+
+</template>
+-->
+
+<script>
+export default {
+name: "SitioWeb",
+data() {
+return {
+slide: 1
+};
+},
+};
+</script>
+
---
+++ src/router/routes.js
@@ -0,0 +1,15 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('pages/SitioWeb.vue')
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
